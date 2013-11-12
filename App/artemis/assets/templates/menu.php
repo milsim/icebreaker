3cdfc9379911b28b59d334667f9d69869db8b849
Johannes Axner <johannes.axner@gmail.com>
2013-11-12 23:44:02 +0100

    Menu tweaks

--- App/artemis/assets/templates/menu.php
+++ App/artemis/assets/templates/menu.php
@@ -1,10 +1,10 @@
 <div class="menu cf">
-	<a href="?page=operation" id="menu_briefing">Operation</a>
-	<a href="?page=messages" id="menu_messages">Messages</a>
-	<a href="?page=intel" id="menu_intel">Intel</a>
-	<a href="?page=map">Map</a>
-	<a href="?page=roster">Roster</a>
-	<a href="?page=files">Files</a>
+	<a href="?page=operation" id="menu_briefing"<?=$page==='operation'?' class="active"':'';?>>Operation</a>
+	<a href="?page=messages" id="menu_messages"<?=$page==='messages'?' class="active"':'';?>>Messages</a>
+	<a href="?page=intel" id="menu_intel"<?=$page==='intel'?' class="active"':'';?>>Intel</a>
+	<a href="?page=map"<?=$page==='map'?' class="active"':'';?>>Map</a>
+	<a href="?page=roster"<?=$page==='roster'?' class="active"':'';?>>Roster</a>
+	<a href="?page=files"<?=$page==='files'?' class="active"':'';?>>Files</a>
 	<a href="?page=logout">Sign off</a>
 
 	<div id="meta">

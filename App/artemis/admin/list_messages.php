01968fa489a57a009cf989e199456a3ac005b173
Sebastian Utbult <sebastian@itiden.se>
2014-02-11 14:08:10 +0100

    Color coding of sent_date in admin/list_messages

--- App/artemis/admin/list_messages.php
+++ App/artemis/admin/list_messages.php
@@ -15,7 +15,13 @@ $x=0;
 while($row=mysql_fetch_assoc($res)) {
 ?>
 <tr style="background-color:#<?php if($x %2 == 0) { echo "fff"; } else { echo "ddd"; } ?>;">
-	<td><?=$row["sent_date"];?>
+	<td><?php 
+	if(date("Y-m-d H:i:s") > $row["sent_date"]) {
+		echo "<b style=\"background-color:#99ff99;\">".$row["sent_date"]."</b>";
+	} else {
+		echo "<b style=\"background-color:#ff9999;\">".$row["sent_date"]."</b>";
+	}
+	?></td>
 	<td><?=$row["subject"];?></td>
 	<td><?php
 		$sql = "SELECT messages_recipients.*,users.name FROM messages_recipients,users WHERE message_id=".$row["id"]." AND users.id=messages_recipients.recipient_id ORDER BY users.name ASC";
@@ -29,7 +35,7 @@ while($row=mysql_fetch_assoc($res)) {
 		}
 	?></td>
 	<td><?=$row["mfrom"];?></td>
-	<td><?=($row["sent"]==1) ? "Sent": "Not sent";?></td>
+	<td><?=($row["sent"]==1) ? "<b style=\"background-color:#99ff99;\">Sent</b>": "Not sent";?></td>
 	<td><a href="edit_message.php?id=<?=$row["id"];?>">Edit/view</a></td>
 </tr>
 <?php
